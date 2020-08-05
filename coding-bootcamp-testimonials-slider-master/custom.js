5b603f19f02be462090bbeb31ccee5f04f56812c
immanuelredd <66283891+immanuelredd@users.noreply.github.com>
2020-08-05 12:36:19 +0100

    added slider

---
+++ coding-bootcamp-testimonials-slider-master/custom.js
@@ -0,0 +1,24 @@
+var index = 1;
+showdiv(index);
+
+function getdiv(n) {
+    showdiv(index += n);
+}
+
+function showdiv(n) {
+    var i;
+    var a = document.getElementsByClassName("testimonial");
+
+    if (n > a.length) {
+        index = 1;
+    }
+
+    if (n < 1) {
+        index = a.length;
+    }
+
+    for (i = 0; i < a.length; i++) {
+        a[i].style.display = "none";
+    }
+    a[index - 1].style.display = "block";
+}
